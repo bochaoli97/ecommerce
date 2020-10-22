7a78282a24a5a35c5052c4fee9490d0f36bd6e60
Bochao Li <bochaoli97@gmail.com>
2020-10-21 17:58:11 -0700

    start to aork on backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
+import data from './data.js';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
+import HomeScreen from './Screens/HomeScreen.js';
+import ProductScreen from './Screens/ProductScreen.js';
 
 function App() {
   const openMenu = () => {
@@ -10,140 +13,43 @@ function App() {
     document.querySelector(".sidebar").classList.remove("open");
   }
   return (
-    <div className="grid-container">
-            <header className="header">
-                <div className="brand">
-                    <button onClick={openMenu}>
-                        &#9776;
-                    </button>
-                    <a href="index.html">Ecommerce</a>
-                </div>
-                <div className="header-links">
-                    <a href="cart.html">Cart</a>
-                    <a href="signin.html">Sign In</a>
-                </div>
-            </header>
-            <aside className="sidebar">
-                <h3>Shopping Categories</h3>
-                <button className="sidebar-close-button" onClick={closeMenu}>X</button>
-                <ul>
-                    <li>
-                        <a  href="index.html">Pants</a>
-                    </li>
-                    <li>
-                        <a  href="index.html">Shirts</a>
-                    </li>
-                </ul>
-            </aside>
-            <main className="main">
-                <div className="content">
-                    <ul className="products">
+    <BrowserRouter>
+        <div className="grid-container">
+                <header className="header">
+                    <div className="brand">
+                        <button onClick={openMenu}>
+                            &#9776;
+                        </button>
+                        <Link to="/">Ecommerce</Link>
+                    </div>
+                    <div className="header-links">
+                        <a href="cart.html">Cart</a>
+                        <a href="signin.html">Sign In</a>
+                    </div>
+                </header>
+                <aside className="sidebar">
+                    <h3>Shopping Categories</h3>
+                    <button className="sidebar-close-button" onClick={closeMenu}>X</button>
+                    <ul>
                         <li>
-                            <div className="product">
-                                <img className="product-image" src="/images/d1.jpg" alt="product" />
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 stars (10 reviews)</div>
-                            </div>
+                            <a  href="index.html">Pants</a>
                         </li>
                         <li>
-                            <div className="product">
-                                <img className="product-image" src="/images/d1.jpg" alt="product" />
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 stars (10 reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="/images/d1.jpg" alt="product" />
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 stars (10 reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="/images/d1.jpg" alt="product" />
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 stars (10 reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="/images/d1.jpg" alt="product" />
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 stars (10 reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="/images/d1.jpg" alt="product" />
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 stars (10 reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="/images/d1.jpg" alt="product" />
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 stars (10 reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="/images/d1.jpg" alt="product" />
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 stars (10 reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="/images/d1.jpg" alt="product" />
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 stars (10 reviews)</div>
-                            </div>
+                            <a  href="index.html">Shirts</a>
                         </li>
                     </ul>
-                </div>            
-            </main>
-            <footer className="footer">
-                All Right Reserved.
-            </footer>
+                </aside>
+                <main className="main">
+                    <div className="content">
+                        <Route path="/products/:id" component={ProductScreen} />
+                        <Route path="/" exact={true} component={HomeScreen} />
+                    </div>            
+                </main>
+                <footer className="footer">
+                    All Right Reserved.
+                </footer>
         </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/data.js
@@ -0,0 +1,44 @@
+export default {
+    products: [
+        {
+            _id: '1',
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: ' Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            _id: '2',
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 70,
+            brand: ' Nike',
+            rating: 4.2,
+            numReviews: 15
+        },
+        {
+            _id: '3',
+            name: 'Best Pant',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 45,
+            brand: ' Nike',
+            rating: 4.0,
+            numReviews: 3
+        },
+        {
+            _id: '4',
+            name: 'Best Pant',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 45,
+            brand: ' Nike',
+            rating: 4.0,
+            numReviews: 3
+        }
+    ]
+}
